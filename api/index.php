1a490ebf7816b6c7e6bce472872c8e447e220087
BuildTools <unconfigured@null.spigotmc.org>
2022-01-14 14:46:02 +0100

    main

--- api/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-error_reporting(0);
-$link = mysqli_connect("localhost", "root", "Minecraft*2015", "droguerie");
-
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}
-?>
