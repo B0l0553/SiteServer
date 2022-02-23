a89fab630e5017000723953be7f76a0faaaa1377
Elven Loret <54502682+B0l0553@users.noreply.github.com>
2022-02-23 19:08:33 +0100

    DELETION >:3

--- api/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-error_reporting(0);
-$link = mysqli_connect("localhost", "tonpere", "03216987", "Koree");
-
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}
-?>
